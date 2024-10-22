85ffa8bca4c0b70bf8082ea9d4cbce4c7621af40
NukeTurtle <stiglic.elvis@gmail.com>
2024-10-22 22:28:51 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
+// so that API is remotely testable by FCC
+let cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -19,12 +15,17 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+// main API endpoint
+app.get("/api/whoami", function (req, res) {
+  let ipaddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  let language = req.headers["accept-language"];
+  let software = req.headers["user-agent"];
+  res.json({ipaddress: ipaddress,
+   "language": language,
+   "software":software});
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+let listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
